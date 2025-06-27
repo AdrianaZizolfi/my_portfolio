d4512fd1a6e60ba72117eb7c1df9e2fb0c37f179
AdrianaZizolfi <adrianazizolfi0@gmail.com>
2025-06-27 18:18:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import LogoSection from "./sections/LogoSection.jsx"
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./sections/Hero.jsx"
+import ShowcaseSection from "./sections/ShowcaseSection.jsx"
+import FeatureCards from "./sections/FeatureCards.jsx"
+import ExperienceSection from "./sections/ExperienceSection.jsx"
+import TechStack from "./sections/TechStack.jsx"
+import Contact from "./sections/Contact.jsx"
+import Footer from "./sections/Footer.jsx"
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <ShowcaseSection />
+    <LogoSection />
+    <FeatureCards />
+    <ExperienceSection />
+    <TechStack />
+    <Contact />
+    <Footer />
+    </>
+
+    
+  )
+}
+
+export default App
